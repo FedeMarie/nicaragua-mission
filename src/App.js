9586b92396aaf87ac30ef65d959ea56cbf4c0ee9
Emmanuella Fede <emmanuella.m.f@gmail.com>
2025-12-22 14:23:27 -0500

    Update to  raised

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const CONFIG = {
   FORMSPREE_ID: 'mblnvpkr',
   
   // Current amount raised - UPDATE THIS when you receive donations!
-  AMOUNT_RAISED: 0,
+  AMOUNT_RAISED: 280,
   
   // Fundraising goal
   GOAL: 2000,
